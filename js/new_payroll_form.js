c085c89d5561307f238cb3882b1df8f24a26b103
akinator001 <aakashgandhi1279@gmail.com>
2020-11-19 14:38:21 +0530

    [aakash] saved employee payroll to local storage

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -79,6 +79,7 @@ window.addEventListener('DOMContentLoaded',(event) => {
 const save = () =>{
     try{
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch(e){
         return;
     }
@@ -121,4 +122,16 @@ const getInputValueById = (id) => {
 const getInputElementValue = (id) => {
     let value = document.getElementById(id).value;
     return value;
+} 
+
+function createAndUpdateStorage(employeePayrollData){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(EmployeePayrollData);
+    } else{
+        employeePayrollList = [employeePayrollData];
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 } 
